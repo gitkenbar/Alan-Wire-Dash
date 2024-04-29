092fcc71e33fdf1f5c188b2752bc830cdda16ac4
gitkenbar <kennethbarker12@gmail.com>
2024-04-29 13:55:21 -0500

    Built sidbar Component+service, reformatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './shared/navigation/navigation.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
 import { SidebarService } from '../../core/services/sidebar.service';
 
 @Component({
-  selector: 'app-navigation',
+  selector: 'app-sidebar',
   standalone: true,
-  imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  imports: [NavigationComponent],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
 })
-export class NavigationComponent {
+export class SidebarComponent {
+
 
   constructor(private sidebar:SidebarService){}
 
