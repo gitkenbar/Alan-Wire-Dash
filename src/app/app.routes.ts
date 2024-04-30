a2d3aecf14c58c9d19f78b40a0e80d2680f0a15e
ssanger <ssanger2020@gmail.com>
2024-04-29 17:29:44 -0700

    moved code necessary from app.component to dashboard component, set up routing to access both dashboard & login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
   }
 ];
