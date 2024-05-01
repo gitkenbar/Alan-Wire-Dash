7825f70b97887f670ba9fac1572357b25e2df533
JeredHuck <jeredhuck@gmail.com>
2024-05-01 15:34:10 -0500

    more methods for http service

--- src/app/core/services/http-data.service.ts
+++ src/app/core/services/http-data.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Department } from '../../shared/models/department.model';
-import { Observable, throwError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
+import { Chart } from '../../shared/models/chart.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,37 @@ export class HttpDataService {
   constructor(private http:HttpClient) { };
 
   getUserDepartments(id:number): Observable<Department[]> {
-    return this.http.get<Department[]>(`${environment.apiUrl}/user/${id}/user_departments/`)
+    return this.http.get<Department[]>(`${environment.apiUrl}/user/${id}/user_departments/`).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  getUserCharts(id:number): Observable<Chart[]> {
+    return this.http.get<Chart[]>(`${environment.apiUrl}/user/${id}/user_charts`).pipe(
+      catchError(this.handleError)
+    )
   }
 
   // ADMIN ONLY
   getAllDepartments() {
-    return this.http.get<Department[]>(`${environment.apiUrl}/departments`)
+    return this.http.get<Department[]>(`${environment.apiUrl}/departments`).pipe(
+      catchError(this.handleError)
+    )
   }
 
-  getUserCharts(id:number) {
-    return this.http.get<Department[]>(`${environment.apiUrl}/user/${id}/user_charts`)
+  addUserCharts(id:number, data:any) {
+    return this.http.post<Chart[]>(`${environment.apiUrl}/user/${id}/user_charts`, data).pipe(
+      catchError(this.handleError)
+    )
   }
+
+  addUserDepartments(id:number, data:any) {
+    return this.http.post<Chart[]>(`${environment.apiUrl}/user/${id}/user_department`, data).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
 
   getAuth() {
 
