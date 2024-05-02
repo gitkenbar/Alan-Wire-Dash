90a0adf58bc0f069fc59496a7e6d48e599194b23
ssanger <ssanger2020@gmail.com>
2024-05-02 11:11:58 -0700

    everything added

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../../shared/components/navigation/navigation.component';
 import { ChartDisplayComponent } from '../../shared/components/chart-display/chart-display.component';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+import { SidebarService } from '../../core/services/sidebar.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavigationComponent, ChartDisplayComponent],
+  imports: [RouterOutlet, NavigationComponent, ChartDisplayComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   title = ' Dashboard - Alan Wire ';
+  constructor(private sidebar:SidebarService){}
+
+  checkSidebar(): boolean{
+    return this.sidebar.isSidebarVisible
+  }
 }
